58dd74a3b538c0e71db9b2e43104590537c2b96e
Lukas <lukas.kubin26@gmail.com>
2021-05-04 18:01:57 +0200

    Week 4 area homework
    TCP client and server

---
+++ src/SWW2/TCPClientArea.java
@@ -0,0 +1,32 @@
+package SWW2;
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class TCPClientArea {
+    public static void main(String[] args) throws Exception {
+        Double radius;
+        String area;
+        System.out.println("Client is Running");
+        System.out.println("Input the radius of the circle: ");
+        // Getting user input
+        Scanner s = new Scanner(System.in);
+        radius = s.nextDouble();
+
+
+        Socket clientSocket = new Socket("127.0.0.1",8020);
+        // getOutpuStream returns an output stream for writing bytes to a socket.
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        // getInputStream() reads bytes from this socket
+        // InputStreamReader converts bytes to characters -> output is a string
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader (clientSocket.getInputStream()));
+        // writeDouble converts a doulbe to bytes saved in a long
+        outToServer.writeDouble(radius);
+        area=inFromServer.readLine();
+        System.out.println("The area for the circle with radius: "+radius+ " is:  "+area);
+        clientSocket.close();
+    }
+
+
+
+}
---
+++ src/SWW2/TCPServerArea.java
@@ -0,0 +1,31 @@
+package SWW2;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.lang.Math.*;
+
+public class TCPServerArea {
+    public static void main(String[] args) throws Exception {
+        double clientRadious;
+        String clientArea;
+
+        ServerSocket welcomeSocket = new ServerSocket(8020);
+        System.out.println("Server is running");
+
+        while(true){
+            // Accepts socket connection
+            Socket connectionSocket = welcomeSocket.accept();
+            // DataInputStream deals with raw bytes BufferedReader deals with characters
+           DataInputStream inFromClient= new DataInputStream(new BufferedInputStream(connectionSocket.getInputStream()));
+           DataOutputStream toClient = new DataOutputStream(connectionSocket.getOutputStream());
+           clientRadious = inFromClient.readDouble();
+           System.out.println("The radius received from the client: "+clientRadious);
+
+           Double area = Math.PI * clientRadious * clientRadious;
+           toClient.writeBytes(area.toString());
+           connectionSocket.close();
+           welcomeSocket.close();
+        }
+    }
+}
